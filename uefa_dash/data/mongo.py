c29f61dfe77fc4750feb73820bfc38d1d4a306a0
Ojedalatronico <Ojedalatronico@gmail.com>
2021-12-29 13:39:09 +0100

    2021 12 29

--- uefa_dash/data/mongo.py
+++ uefa_dash/data/mongo.py
@@ -1,2 +1,11 @@
 from pymongo import MongoClient
-from dotenv 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+user=os.getenv("MONGO_USER")
+password=os.getenv("MONGO_PASS")
+
+URL=f"mongodb+srv://{user}:{password}@uefa.kgpdy.mongodb.net"
+
+db=MongoClient().get_database("UEFA")
