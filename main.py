9ea4bf951fae5d31d8ee92834c990426b30939dc
Ojedalatronico <Ojedalatronico@gmail.com>
2022-01-09 14:58:25 +0100

    6 fix

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from api.router import players,team,league,matches
+from router import players,team,league
 
 app=FastAPI()
 
 app.include_router(team.router)
 app.include_router(league.router)
 app.include_router(players.router)
-app.include_router(matches.router)
 
 @app.get("/")
 def root():
